290d1781f6956cf1c5e9b57907e5b53c871334bc
Ha Nhut Tuan <133622164+hntuan7684@users.noreply.github.com>
2024-09-11 09:47:49 +0700

    Standardlize JSON responses

--- src/app.js
+++ src/app.js
@@ -1,20 +1,18 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 
-const contactsRouter = require('./routes/contacts.router')
+const JSend = require('./jsend');
+const contactsRouter = require('./routes/contacts.router');
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    return res.json({
-        message: 'ok'
-    })
-})
+    return res.json(JSend.success());
+});
 
-contactsRouter.setup(app)
-
-module.exports = app
+contactsRouter.setup(app);
+module.exports = app;
---
+++ src/jsend.js
@@ -0,0 +1,54 @@
+/**
+* @param {object | null} data
+* @returns {{status: 'success', data: object | null}}
+*/
+function success(data = null) {
+return {
+status: 'success',
+data: data,
+};
+}
+
+/**
+ * @param {string} message - The error message describing the failure.
+ * @param {object | null} [data=null] - Optional additional data related to the failure. Defaults to null if not provided.
+ * @returns {{status: 'fail', message: string, data: object | null}} 
+ */
+function fail(message, data = null) {
+    if (data) {
+        return {
+            status: 'fail',
+            message,
+            data,
+        };
+    }
+    return {
+        status: 'fail',
+        message,
+    };
+}
+
+/**
+ * 
+ * @param {string} message 
+ * @param {*object | null} data 
+ * @returns {{status: 'error', message: string, data: object | undefined}}
+ */
+function error(message, data = null) {
+    if (data) {
+        return {
+            status: 'error',
+            message,
+            data,
+        };
+    }
+    return {
+        status: 'error',
+        message,};
+}
+
+module.exports = {
+    success,
+    fail,
+    error,
+}
